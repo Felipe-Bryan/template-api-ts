bb58a5815a647ebdde638fc913793d85c8865dc9
Felipe Bryan <felipe.bryan@live.com>
2023-08-23 10:43:53 -0300

    comentario removido

--- src/main/config/express.config.ts
+++ src/main/config/express.config.ts
@@ -20,21 +20,3 @@ export const createApp = () => {
 
   return app;
 };
-
-// export class Server {
-//   public static create() {
-//     const app = express();
-//     app.use(express.json());
-//     app.use(cors());
-
-//     // app.use('/path', routerFile);
-
-//     return app;
-//   }
-
-//   public static listen(app: Express) {
-//     app.listen(process.env.PORT, () => {
-//       console.log('Servidor rodando na porta ' + process.env.PORT + '!');
-//     });
-//   }
-// }
